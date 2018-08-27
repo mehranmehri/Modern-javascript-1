c479d26c6224a3970a34149a93759d8c4fb8ab55
Valentin Stanciu <stanciu327@gmail.com>
2018-08-27 17:52:21 +0100

    second video finished

--- app.js
+++ app.js
@@ -4,3 +4,21 @@
 */
 
 console.log("hello world");
+console.log(123);
+console.log(true);
+var greeting = "hello";
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2, c: 3 });
+console.table({ a: 2, b: 3 });
+console.error("This is an error");
+console.clear();
+console.warn("This is a warning");
+
+console.time("Hello");
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.timeEnd("Hello");
