a1de454bb504257402584e28d6f4d0e4291dd022
Valentin Stanciu <stanciu327@gmail.com>
2018-08-28 08:06:49 +0100

    date and time

---
+++ dateTime.js
@@ -0,0 +1,26 @@
+let val;
+
+const today = new Date();
+let birthday = new Date("9-10-1992 11:25:00");
+birthday = new Date("September 10 1981");
+birthday = new Date("9/10/1981");
+
+val = today.getMonth(); //FYI it's 0 based so January is 0
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+
+val = today.getTime();
+
+birthday.setMonth(2);
+birthday.setDate(12);
+birthday.setFullYear(1986);
+birthday.setHours(3);
+birthday.setMinutes(30);
+birthday.setSeconds(25);
+
+console.log(typeof val);
+console.log(birthday);
--- objectLiterals.js
+++ objectLiterals.js
@@ -31,6 +31,7 @@ val = person.getBirthYear();
 console.log(val);
 
 const people = [{ name: "john", age: 30 }, { name: "smith", age: 25 }];
+
 for (let i = 0; i < people.length; i++) {
   console.log(people[i].name);
 }
