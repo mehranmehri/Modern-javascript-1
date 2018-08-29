3cbb3daffddb5d3a1aa076dd956cb7beaad68e00
Valentin Stanciu <stanciu327@gmail.com>
2018-08-29 10:28:55 +0100

    task list part 1

--- Task-list-project/tasklist/app.js
+++ Task-list-project/tasklist/app.js
@@ -1,45 +1,45 @@
 // Define UI Vars
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
 
 // Load all event listeners
 loadEventListeners();
 
-// Load all event listeners
+//Load all event listeners
 function loadEventListeners() {
-  // Add task event
-  form.addEventListener('submit', addTask);
+  //Add task event
+  form.addEventListener("submit", addTask);
 }
 
-// Add Task
+//Add task
 function addTask(e) {
-  if(taskInput.value === '') {
-    alert('Add a task');
+  if (taskInput.value === "") {
+    alert("Add a task");
   }
 
-  // Create li element
-  const li = document.createElement('li');
-  // Add class
-  li.className = 'collection-item';
-  // Create text node and append to li
+  //Create li element
+  const li = document.createElement("li");
+  //Add class
+  li.className = "collection-item";
+  //Create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
-  // Create new link element
-  const link = document.createElement('a');
-  // Add class
-  link.className = 'delete-item secondary-content';
-  // Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Append the link to li
+  //create new link element
+  const link = document.createElement("a");
+  //add class
+  link.className = "delete-item secondary-content";
+  //add icon html
+  link.innerHTML = "<i class='fa fa-rmeove'></i>";
+  //append the link to li
   li.appendChild(link);
 
-  // Append li to ul
+  //append li to ul
   taskList.appendChild(li);
 
-  // Clear input
-  taskInput.value = '';
+  //clear input
+  taskInput.value = "";
 
   e.preventDefault();
 }
